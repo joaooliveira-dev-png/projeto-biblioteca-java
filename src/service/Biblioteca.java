51a66ad0ff436c8394ce37b40fc07417024dfaf0
Joao Vitor <almeidaoliveira9697@gmail.com>
2026-04-06 15:17:33 -0300

    feat: implementa regras de empéstimo e devolução de livros

--- src/service/Biblioteca.java
+++ src/service/Biblioteca.java
@@ -8,6 +8,7 @@ import model.Usuario;
 public class Biblioteca {
     private List<Livro> livros = new ArrayList<>();
     private List<Usuario> usuarios = new ArrayList<>();
+    private boolean disponivel;
     
     public void adicionarLivro(Livro livro) {
     livros.add(livro);
@@ -17,6 +18,24 @@ public class Biblioteca {
         usuarios.add(usuario);
     }
 
+    public void emprestarLivro(Livro livro, Usuario usuario){
+        if(!livro.isDisponivel()){
+            System.out.println("O livro está indisponível no momento. Aguarde a sua devolução");
+        } else {
+            livro.setDisponivel(false);
+            System.out.println(usuario + " seu emprestimo foi realizado com sucesso");
+        }
+    }
+    
+    public void devolverLivro(Livro livro){
+       if(livro.isDisponivel()){
+           System.out.println("Este livro já foi devolvido! Obrigado!");
+       }else{
+           livro.setDisponivel(true);
+           System.out.println("Livro devolvido com sucesso");
+       }
+    }
+    
     @Override
     public String toString() {
         return "Livros: " + livros.size() +
